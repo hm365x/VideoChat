6c39f5affa932d0fb6dda20fc77ae93b5baa5730
mhuang <mhuang@art-coding.com>
2025-05-09 14:00:12 +0800

    添加nltkdl.py, 方便遇到报错 Resource averaged_perceptron_tagger_eng not found 时下载数据

---
+++ nltkdl.py
@@ -0,0 +1,5 @@
+#运行app.py时遇到nltk相关报错, 执行本文件一次即可, 会自动下载nltk数据到/root/nltk_data目录下
+import nltk
+nltk.download('averaged_perceptron_tagger_eng')
+#nltk.download('averaged_perceptron_tagger')
+#nltk.download('cmudict')
